104e9ff96a35961ed1628ac2aed49ea03468a4db
Rohit Parida <rohitparida27@gmail.com>
2017-11-09 16:46:14 +0530

    Initial

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//Increment-Button
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+//Reset-Button
+$('').click(function(){
+    [0].reset();
+});
+
+// Registering ServiceWorker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
